a9234af65694c623e3e682525d207b52090e850e
neveisa <smpdmohanty@gmail.com>
2020-09-16 23:31:00 -0700

    finished hw2

--- hw2/unblur.py
+++ hw2/unblur.py
@@ -1,12 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
-
 import scipy.sparse as sp
 import scipy.sparse.linalg as splinalg
-
-
-
 import time
 
 start = time.time()
@@ -15,8 +11,6 @@ blurryimg = loadmat('blurryimage.mat')['blurryimage']
 B = blurryimg.flatten()
 
 #print('IMG DATATYPE:',type(blurryimg))
-
-
 # extend image by inserting a black boundary box
 '''
 hborder = np.array([[0]*64])
@@ -26,16 +20,12 @@ img=np.concatenate((hborder,img),axis=0)
 img=np.concatenate((img,vborder.T),axis=1)
 img=np.concatenate((vborder.T,img),axis=1)
 '''
-
 #print('IMG SHAPE:',blurryimg.shape)
-
 
 plt.imshow(blurryimg,cmap='gray')
 plt.savefig('blurryimage.png')
-#plt.show()
 
 filters = []
-
 for i in range(64):
     for j in range(64):
         z = np.zeros([64,64])
@@ -56,7 +46,6 @@ for i in range(64):
         if (k:=j-1)>=0:
             z[i,k]=1/8
 
-
         filters.append(z.flatten())
 
 
@@ -71,22 +60,25 @@ print(F[4*64+3][:(6*64)].reshape(6,64))
 import sys
 method=sys.argv[1]
 
-if method=='naive':
+if method=='compare':
+    method='naive|spinv|splu'
+
+if 'naive' in method:
     
     #takes 9s to invert and get result
     I = np.linalg.inv(F) @ B
+    Inaive = I.copy()
 
-
-if method =='spinv':
+if 'spinv' in method:
     
     #convert to sparse
     FCSC = sp.csc_matrix(F)
     
     #takes 9s to invert as well, maybe saves memory when using sparse, but not compute
     I = splinalg.inv(FCSC).dot(B)
+    Ispinv = I.copy()
 
-
-if method == 'splu':
+if 'splu' in method:
     
     #convert to sparse
     FCSC = sp.csc_matrix(F)
@@ -95,22 +87,27 @@ if method == 'splu':
     # takes 1s to do lu decomp
     lu_object = splinalg.splu(FCSC)
 
-
     #takes 6.5s
     # note result is transposed
     I = lu_object.solve(B) # recover original image I
-
+    Isplu = I.copy()
 
 # Note I was supposed to be a column vector but solve returns transpose, hence we have a row vector.
 # We don't care so much as we have a vector, we need to care if we had given a matrix to solve instead of the vector B
 
-recoveredoriginalsharpimage = I.reshape(64,64)
-plt.imshow(recoveredoriginalsharpimage,cmap='gray')
-plt.savefig(f'recoveredoriginalsharpimagei-{method}.png')
-#plt.show()
-
-
 end = time.time()
 
-print(f'METHOD:{method} | took -> {end - start} seconds...\n')
+if method!='naive|spinv|splu':
+
+    recoveredoriginalsharpimage = I.reshape(64,64)
+    plt.imshow(recoveredoriginalsharpimage,cmap='gray')
+    plt.savefig(f'recoveredoriginalsharpimage-{method}.png')
+    #plt.show()
+    
+    print(f'METHOD:{method} | took -> {end - start} seconds...\n')
+
+else:
+    print(f'DIFF naive vs spinv ->',np.linalg.norm(Inaive - Ispinv))
+    print(f'DIFF naive vs splu ->',np.linalg.norm(Inaive - Isplu))
+    print(f'DIFF spinv vs splu ->',np.linalg.norm(Ispinv - Isplu))
 
